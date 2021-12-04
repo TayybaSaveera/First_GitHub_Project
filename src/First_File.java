449811290bf0e50cffe40040c182e7b18ec5c8aa
TayybaSaveera <95464545+TayybaSaveera@users.noreply.github.com>
2021-12-04 16:25:48 +0500

    my first commit

---
+++ src/First_File.java
@@ -0,0 +1,12 @@
+
+public class First_File {
+
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Tayyba";
+		String rollno = "20i-2314";
+		System.out.println("Name: " + name);
+		System.out.println("RollNo: " + rollno);
+		}
+		}
+
